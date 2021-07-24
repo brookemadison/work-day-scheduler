2f884a5933091a368cd61f3e7d84218c7ac68e09
brookemadison <brookeperry611@gmail.com>
2021-07-24 12:07:12 -0400

    added current date & time + changed colors

--- script.js
+++ script.js
@@ -1,2 +1,9 @@
 // Current time and date
+var DateTime = luxon.DateTime
+var time = DateTime.now().toLocaleString(DateTime.DATETIME_SHORT)	
+console.log (time) 
+
+var dateElement = document.getElementById ("currentDay")
+
+dateElement.textContent = time
 
